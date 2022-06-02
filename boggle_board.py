c2c9b1819319e6482c790e41374dcac41c6bb72e
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-06-02 17:04:47 -0400

    works but needs formatting

--- boggle_board.py
+++ boggle_board.py
@@ -1,6 +1,38 @@
+import random 
+from pprint import pprint
+
 class BoggleBoard:
-  
+
+  dice_list = []
+  for i in range(16):
+    die = []
+    for x in range(6):
+      die.append((chr(random.randint(ord('A'), ord('Z')))))
+    dice_list.append(die)
+    random.shuffle(dice_list)
+
   def __init__(self):
-  
+    board = ''
+    for x in range(16):
+      if x % 4 == 0:
+        board += '\n'
+      board += '_'
+    board += '\n'
+    print(*board)
 
   def shake(self):
+    board = ''
+    new_letter = ''
+    for i,x in enumerate(BoggleBoard.dice_list):
+      if i % 4 == 0:
+        board += '\n'
+      new_letter = x[random.randint(0,5)]
+      if new_letter == 'Q':
+        board += new_letter + 'u '
+      else:
+        board += new_letter + '  ' 
+    board += '\n'
+    print(*board)
+
+board_new = BoggleBoard()
+board_new.shake()
